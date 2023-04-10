02347e12d903330e526fe0955be611421d0f1e65
SephMon <kevinosullivan25@gmail.com>
2023-04-10 20:58:10 +0100

    Getting started still

--- Main.py
+++ Main.py
@@ -1,2 +1,19 @@
-print("Hello world")
-print("hi mate ")
+from RockPaperScissors import RockPaperScissors
+
+r = RockPaperScissors()
+
+
+def welcome_message():
+    print("*************************")
+    print("*       Welcome to      *")
+    print("*    RockPaperScissors  *")
+    print("*************************")
+
+
+welcome_message()
+
+while not r.game_over:
+    r.player_choice = r.get_player_choice()
+    r.out_of_bounds()
+
+
