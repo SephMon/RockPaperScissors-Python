d4557085a03f10742fd84840480f03c1b4848c9d
SephMon <kevinosullivan25@gmail.com>
2023-04-10 23:12:24 +0100

    Added function bodies,no functionality yet.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -37,6 +37,58 @@ class RockPaperScissors:
     
     def __init__(self):
         self.turn = self.get_number_of_games()
+        self.game_over = False
+        self.player_score = 0
+        self.computer_score = 0
+        self.play_again = self.ask_to_play_again
         
+    def get_player_choice(self):
+        pass
     
+    def check_for_out_of_bounds_choice(self):
+        pass
     
+    def get_number_of_games(self):
+        pass
+
+    def set_number_of_games(self):
+        pass
+
+    def check_number_of_rounds(self):
+        pass
+
+    def get_computer_choice(self):
+        pass
+
+    def print_out_computer_choice(self):
+        pass
+
+    def check_who_won(self):
+        pass
+
+    def print_scores(self):
+        pass
+
+    def check_score(self):
+        pass
+
+    def ask_player_to_play_again(self):
+        pass
+
+    def ask_to_play_again(self):
+        pass
+
+    def check_if_choice_is_out_of_bounds(self):
+        pass
+
+    def check_player_input(self):
+        pass
+
+    
+
+
+
+
+
+
+
